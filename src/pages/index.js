7c69d3c17aeee3a76789141a20ffa92ed48e7af4
torokokoo <torokokov7@gmail.com>
2025-10-20 14:47:14 -0300

    modified colors and style

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Ir a la guia 🚀
           </Link>
         </div>
       </div>
